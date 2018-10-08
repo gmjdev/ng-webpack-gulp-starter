0c889430d35d38c3266668d24c55b5af0a5a3d4b
gmjDev <gm.jdev@gmail.com>
2018-10-08 23:46:58 +0530

    Correct report issue

--- config/test/karma.conf.js
+++ config/test/karma.conf.js
@@ -10,9 +10,10 @@ import {
 } from '../webpack.test';
 const cwd = process.cwd();
 const appConfig = IoUtil.readJsonFile(path.join(cwd, 'app-config.json'));
-const env = appProgram.validateAndGetEnvironment(appConfig.environment);
-const baseDir = path.join(cwd, appConfig.source.buildDir, appConfig.source.reportsDir,
-    appConfig.test.junit.reportDir);
+const baseDir = path.join(appConfig.source.buildDir, appConfig.source.reportsDir,
+    appConfig.test.report.coverageDir);
+const junitDir = path.join(cwd, appConfig.source.buildDir, appConfig.source.reportsDir,
+    appConfig.test.report.junitDir);
 
 export default function (config) {
     config.set({
@@ -22,10 +23,10 @@ export default function (config) {
 
         frameworks: appConfig.test.junit.frameworks,
 
-        reporters: ["progress", "kjhtml", "coverage", 'remap-coverage'],
+        reporters: appConfig.test.junit.reporters,
 
         preprocessors: {
-            '../../src/test.ts': ["webpack", "sourcemap", "coverage"]
+            '../../src/test.ts': appConfig.test.junit.preprocessors
         },
 
         files: appConfig.test.junit.files,
@@ -38,30 +39,26 @@ export default function (config) {
             //'errors-only' - print error onlys
             stats: 'normal'
         },
+
         webpackServer: {
             noInfo: true
         },
+
         client: {
             clearContext: false
         },
+
+        htmlReporter: {
+            outputFile: path.join(junitDir, appConfig.test.report.junitReportFileName),
+            pageTitle: 'Unit Tests Result',
+            subPageTitle: '',
+            groupSuites: true,
+            useCompactStyle: true,
+            useLegacyStyle: true
+        },
+
         coverageReporter: {
             type: 'in-memory',
-            dir: path.join(cwd, appConfig.source.buildDir, appConfig.source.reportsDir,
-                appConfig.test.junit.reportDir),
-            /*reporters: [{
-                type: 'text-summary'
-            }, {
-                type: 'html',
-                subdir: 'html'
-            }, {
-                type: 'cobertura',
-                subdir: '.',
-                file: 'cobertura-coverage.xml'
-            }, {
-                type: 'lcovonly',
-                subdir: '.',
-                file: 'lcovonly-coverage.txt'
-            }],
             fixWebpackSourcePaths: true,
             /*check: {
                 global: {
@@ -83,7 +80,8 @@ export default function (config) {
         },
         remapCoverageReporter: {
             html: path.join(baseDir, 'html'),
-            cobertura: path.join(baseDir, 'cobertura.xml')
+            cobertura: path.join(baseDir, appConfig.test.report.coberturaFileName),
+            lcovonly: path.join(baseDir, appConfig.test.report.lcovonlyFileName),
         },
         mime: {
             'text/x-typescript': appConfig.test.junit.tsMime
